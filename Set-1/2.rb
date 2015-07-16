d8b21f1d11c2e4c10a6e18ab2aa383da59113da6
Ayush Goel <ayushgoel111@gmail.com>
2015-07-16 23:49:07 +0530

    Fix xor method to be able to take large strings as input

--- Set-1/2.rb
+++ Set-1/2.rb
@@ -6,9 +6,11 @@ end
 
 public
 def xor(str1, str2)
-  h1 = treatAsHex str1
-  h2 = treatAsHex str2
-  (h1 ^ h2).to_s(16)
+  str1.scan(/\w\w/).zip(str2.scan(/\w\w/)).map { |s1_c, s2_c|
+    h1 = treatAsHex s1_c
+    h2 = treatAsHex s2_c
+    (h1 ^ h2).to_s(16).rjust(2, '0')
+  }.join
 end
 
 def help
