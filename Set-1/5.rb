f623e1e3ba15b151b049398b012e6251d0504e84
Ayush Goel <ayushgoel111@gmail.com>
2015-07-16 23:49:30 +0530

    Add solution for challenge 5

--- Set-1/5.rb
+++ Set-1/5.rb
@@ -2,11 +2,18 @@
 
 require_relative '2'
 
-InputString = "Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal"
+def decrypt(input, key)
+    inputInHex = input.unpack('H*').last
+    keyInHex = key.unpack('H*').last
+    fullKey = (keyInHex * ((inputInHex.length / keyInHex.length) + 1))[0, inputInHex.length]
+    2.xor(inputInHex, fullKey)
+end
+
 
 def main
-    puts InputString
+    input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+    key = "ICE"
+    puts decrypt(input, key)
 end
 
 main if __FILE__ == $0
