cd1ee8e20de1cadfb6ff0f727d87d6b03820431f
Ayush Goel <ayushgoel111@gmail.com>
2015-07-16 23:48:38 +0530

    Add file for challenge 5

---
+++ Set-1/5.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require_relative '2'
+
+InputString = "Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"
+
+def main
+    puts InputString
+end
+
+main if __FILE__ == $0
